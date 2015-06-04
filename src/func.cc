82634016da5064d45db2efca0c9525ccf8419abb
Yann Kurzo <ya_246@hotmail.com>
2015-06-04 21:20:19 +0200

    Automake with include folder

---
+++ src/func.cc
@@ -0,0 +1,8 @@
+#include "func.h"
+
+#include <iostream>
+
+void show(void)
+{
+    std::cout << "Func" << std::endl;
+}
---
+++ src/main.cc
@@ -0,0 +1,12 @@
+#include "func.h"
+#include <config.h>
+
+#include <iostream>
+
+int main (void)
+{
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "This is " << PACKAGE_STRING << "." << std::endl;
+    show();
+    return 0;
+}
