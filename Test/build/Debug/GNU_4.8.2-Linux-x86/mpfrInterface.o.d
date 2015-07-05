84f3de089c6febc0306885c2f119b463f0bc74e4
Yann Kurzo <ya_246@hotmail.com>
2015-07-05 22:09:11 +0200

    Delete tmp project

--- Test/build/Debug/GNU_4.8.2-Linux-x86/mpfrInterface.o.d
+++
@@ -1,6 +0,0 @@
-build/Debug/GNU_4.8.2-Linux-x86/mpfrInterface.o: mpfrInterface.cc \
- mpfrInterface.h config.h
-
-mpfrInterface.h:
-
-config.h:
--- Test/build/Debug/GNU_4.8.2-Linux-x86/util.o.d
+++
@@ -1,3 +0,0 @@
-build/Debug/GNU_4.8.2-Linux-x86/util.o: util.cc util.h
-
-util.h:
